017ec90ac669ce1c1bb456f92e44412b529f34b3
Danya Khalikov <sdc_ac@mail.ru>
2021-12-10 19:25:45 +0300

    Lab1

---
+++ Lab1/lab1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char *sub = "Hello, world!";
+    int x = 666;
+    printf("%s\n", sub); // Вывод строки
+    printf("%d\n", x); // Вывод числа
+    return 0;
+}
